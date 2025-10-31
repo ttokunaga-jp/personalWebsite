07125a2ab02e870b0d7fa547bd73dcb7328d5b7d
takumi-1234 <tkngtkmrwgnnns@gmail.com>
2025-11-01 02:15:21 +0900

    GitHubActionエラーの修正

--- deploy/docker/nginx/entrypoint.sh
+++ deploy/docker/nginx/entrypoint.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
-set -e
+set -ex
 
 export PORT=${PORT:-8080}
+echo "--- Substituting PORT: ${PORT} ---"
 
+echo "--- Checking permissions ---"
+ls -ld /etc/nginx/conf.d
+ls -l /etc/nginx/conf.d/default.conf.template
+
+echo "--- Generating nginx config ---"
 envsubst '${PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
-exec nginx -g 'daemon off;'
+echo "--- Generated /etc/nginx/conf.d/default.conf ---"
+cat /etc/nginx/conf.d/default.conf
+echo "---------------------------------------------"
+
+echo "--- Starting nginx ---"
+nginx -g 'daemon off;'
+echo "--- nginx exited with code $? ---"

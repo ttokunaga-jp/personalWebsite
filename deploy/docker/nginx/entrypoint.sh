6a52b8e6989344e211b3d6e3446820c046ec3a5d
takumi-1234 <tkngtkmrwgnnns@gmail.com>
2025-11-01 01:25:38 +0900

    GitHubActionエラーの修正

--- deploy/docker/nginx/entrypoint.sh
+++ deploy/docker/nginx/entrypoint.sh
@@ -4,8 +4,5 @@ set -e
 # PORT環境変数が未設定の場合、デフォルトで8080を使用
 export PORT=${PORT:-8080}
 
-# envsubstで設定ファイル内の${PORT}を実際の値に置換し、新しい設定ファイルを生成
-envsubst '${PORT}' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/generated_default.conf
-
-# 生成した設定ファイルを使用してNginxをフォアグラウンドで実行
-exec nginx -g 'daemon off;' -c /etc/nginx/conf.d/generated_default.conf
+# envsubstで設定ファイル内の変数を置換し、標準出力に出力
+envsubst '${PORT}' < /etc/nginx/conf.d/default.conf
